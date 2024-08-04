0b855b96cc9031e7c5728e5f3425633eff185b27
Oleg Mikhailov <o.v.mikhailov01@yandex.ru>
2024-08-04 20:01:34 +0300

    Changing the site logic (#2)

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,22 @@
-let requestURL = "https://mikhv.github.io/data.json";
-let request =  new XMLHttpRequest();
+const pagePathElem = document.getElementById('pagePath');
+const pageTitleElem = document.getElementById('pageTitle');
+const iframe = document.getElementById('iframe');
 
-request.open("GET", requestURL);
+fetch("https://mikhv.github.io/data.json", {method: "GET"}) // '/data.json'
+.then(function(response) {
+  return response.json();
+}).then(function(json) {
+  console.log('parsed json', json)
 
-requestAnimationFrame.responseType = "json";
-request.send();
 
-let jsonObj;
-request.onload = function() {
-    jsonObj = request.response;
+}).catch(function(ex) {
+  console.log('parsing failed', ex)
+});
+
+function fillNavMenu() {
+
 }
 
-console.log(jsonObj);
+function openPage() {
+
+}
